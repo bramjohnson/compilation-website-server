fa93d3c3fc79954a8132396418df462aec4b8a3c
Bram Johnson <89540826+bramjohnson@users.noreply.github.com>
2026-04-03 10:25:09 -0400

    DB migrations

--- src/scripts/extract_nm_playlist_response.ts
+++ src/scripts/extract_nm_playlist_response.ts
@@ -1,4 +1,5 @@
 import { readdirSync, readFileSync, writeFileSync } from "node:fs";
+import { createPrismaClient } from "../prisma";
 
 interface RawNintendoMusicMediaPayload {
   durationMillis: number;
@@ -19,20 +20,30 @@ interface RawNintendoMusicGame {
   name: string;
 }
 
-interface RawNintendoMusicResponse {
+interface RawOfficialPlaylistsResponse {
   game: RawNintendoMusicGame;
   tracks: RawNintendoMusicTrack[];
 }
 
-function getRawNintendoMusicRespones(): [string[], RawNintendoMusicResponse[]] {
-  const rawResponseFilePaths = readdirSync("./mitmed/raw/");
+interface RawRelatedPlaylistsResponse {
+  allPlaylist: {
+    id: string;
+    tracksNum: number;
+  };
+}
+
+function getRawNintendoMusicRespones(): [
+  string[],
+  RawOfficialPlaylistsResponse[],
+] {
+  const rawResponseFilePaths = readdirSync("./mitmed/raw/officialPlaylists/");
   console.log(rawResponseFilePaths);
   const rawRespones = rawResponseFilePaths.map((filePath) =>
-    readFileSync(`./mitmed/raw/${filePath}`),
+    readFileSync(`./mitmed/raw/officialPlaylists/${filePath}`),
   );
   const rawJsonResponses = rawRespones.map((response) =>
     JSON.parse(response.toString()),
-  ) as RawNintendoMusicResponse[];
+  ) as RawOfficialPlaylistsResponse[];
   return [rawResponseFilePaths, rawJsonResponses];
 }
 
@@ -53,7 +64,7 @@ interface ExtractedNintendoMusic {
 }
 
 function extractedNintendoMusicFromRawNintendoMusic(
-  rawNintendoMusic: RawNintendoMusicResponse,
+  rawNintendoMusic: RawOfficialPlaylistsResponse,
 ): ExtractedNintendoMusic {
   return {
     game: {
@@ -73,6 +84,60 @@ function main() {
   const extracteds = rawResponses.map((response) =>
     extractedNintendoMusicFromRawNintendoMusic(response),
   );
+
+  const prismaClient = createPrismaClient();
+  extracteds.forEach(async (extracted) => {
+    // Try to automatically associate to existing game
+    const gameGuess =
+      await prismaClient.nintendoMusicLibraryGameAlbum.findFirst({
+        where: {
+          name: extracted.game.name,
+        },
+      });
+
+    if (gameGuess === null) {
+      console.log("No good guess ):");
+      return;
+    }
+
+    console.log(`Updating ${gameGuess.name} with ID ${extracted.game.id}`);
+    prismaClient.nintendoMusicLibraryGameAlbum.update({
+      where: {
+        id: gameGuess.id,
+      },
+      data: {
+        nintendoMusicId: extracted.game.id,
+      },
+    });
+
+    extracted.tracks.forEach(async (track) => {
+      // Try to automatically associate to existing track
+      const trackGuess = await prismaClient.nintendoMusicLibraryTrack.findFirst(
+        {
+          where: {
+            title: track.name,
+            nintendoMusicLibraryGameAlbumId: gameGuess.id,
+          },
+        },
+      );
+
+      if (trackGuess === null) {
+        console.log(`No good track guess for ${track.name}`);
+        return;
+      }
+
+      console.log(`Updating ${trackGuess.title} with ID ${track.id}`);
+      prismaClient.nintendoMusicLibraryTrack.update({
+        where: {
+          id: trackGuess.id,
+        },
+        data: {
+          nintendoMusicId: track.id,
+        },
+      });
+    });
+  });
+
   extracteds.map((extracted, idx) => {
     const rawPath = rawPaths[idx];
     writeFileSync(`./mitmed/extracted/${rawPath}`, JSON.stringify(extracted));
